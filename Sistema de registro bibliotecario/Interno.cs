9c4e037a46f699b941d61d526201bc681ecfc2b2
AndresMcDev <118392919+AndresMcDev@users.noreply.github.com>
2022-11-16 15:33:20 -0500

    Update Interno.cs

--- Sistema de registro bibliotecario/Interno.cs
+++ Sistema de registro bibliotecario/Interno.cs
@@ -20,30 +20,30 @@ namespace Sistema_de_registro_bibliotecario
             
             InitializeComponent();
 
-            string RutaJsonFile = File.ReadAllText("Libros.json");//Se obtiene en una variable la ruta del archivo Json que contiene los primeros libros
-            DataTable dt = (DataTable)JsonConvert.DeserializeObject(RutaJsonFile, typeof(DataTable));//Se deseariliza el aarchivo en un dataTable "dt"
+            string RutaJsonFile = File.ReadAllText("Libros.json");//Se obtiene en una variable el contenido del archivo Json que contiene los primeros libros
+            DataTable dt = (DataTable)JsonConvert.DeserializeObject(RutaJsonFile, typeof(DataTable));//Se deseariliza la información en un dataTable "dt"
             
             for(int i =0; i<dt.Rows.Count; i++)
             {
-                _Libro MiLibro = new _Libro(); //Se crean objetos 
+                _Libro MiLibro = new _Libro(); //Se crean un objeto por cada row del dataTable 
 
-                MiLibro.Libro = dt.Rows[i].ItemArray[0].ToString(); //Se llenan los campos de objeto con los rows del dataTable
-                MiLibro.Autor = dt.Rows[i].ItemArray[1].ToString(); //Se llenan los campos de objeto con los rows del dataTable
-                MiLibro.Ubicacion = dt.Rows[i].ItemArray[2].ToString(); //Se llenan los campos de objeto con los rows del dataTable
-                MiLibro.Cantidad = dt.Rows[i].ItemArray[3].ToString(); //Se llenan los campos de objeto con los rows del dataTable
+                MiLibro.Libro = dt.Rows[i].ItemArray[0].ToString(); //Se llenan los campos del objeto con los rows del dataTable
+                MiLibro.Autor = dt.Rows[i].ItemArray[1].ToString(); 
+                MiLibro.Ubicacion = dt.Rows[i].ItemArray[2].ToString(); 
+                MiLibro.Cantidad = dt.Rows[i].ItemArray[3].ToString(); 
 
-                MisLibros.Add(MiLibro); //Se agrega el abjetos a la lista "MisLibros"
+                MisLibros.Add(MiLibro); //Se agrega el abjeto a la lista "MisLibros"
             }
 
-            DtgLibros.DataSource = MisLibros;//Se actualiza el datasource del datagrid con la información de la lista "MisLibros"
+            DtgLibros.DataSource = MisLibros;//Se actualiza el datagrid con la información de la lista "MisLibros"
         }
 
         private void BtnRegistrar_Click(object sender, EventArgs e)
         {
-            _Libro MiLibro = new _Libro();//Crear objeto (Libro)
+            _Libro MiLibro = new _Libro();//Se crea un objeto (Libro)
 
             
-            if(ValidarNombre() == false)//Validar controles
+            if(ValidarNombre() == false)//Validar controles (que no esten vacios)
             {
                 return;
             }
@@ -60,12 +60,12 @@ namespace Sistema_de_registro_bibliotecario
                 return;
             }
   
-            MiLibro.Libro = TxtLibro.Text;//LLenar el objeto con los valores ingresados en los controles
+            MiLibro.Libro = TxtLibro.Text;//Dat valores a las propiedades del objeto
             MiLibro.Autor = TxtAutor.Text;
             MiLibro.Ubicacion = TxtUbicacion.Text;
             MiLibro.Cantidad = TxtCantidad.Text;
             
-            MisLibros.Add(MiLibro);//Agregar el abjeto a la lista de MisLibros
+            MisLibros.Add(MiLibro);//Agregar el abjeto a la lista "MisLibros"
     
             DtgLibros.DataSource = null;//Actualizar datagrid con los objetos de lista
             DtgLibros.DataSource = MisLibros;
@@ -145,7 +145,7 @@ namespace Sistema_de_registro_bibliotecario
         {
             if (string.IsNullOrEmpty(TxtCantidad.Text))
             {
-                EpvError.SetError(TxtCantidad, "Debe ingresar el autor");
+                EpvError.SetError(TxtCantidad, "Debe ingresar la cantidad");
                 return false;
             }
             else
@@ -159,7 +159,7 @@ namespace Sistema_de_registro_bibliotecario
         {
             if (string.IsNullOrEmpty(TxtUbicacion.Text))
             {
-                EpvError.SetError(TxtUbicacion, "Debe ingresar el autor");
+                EpvError.SetError(TxtUbicacion, "Debe ingresar la ubicación");
                 return false;
             }
             else
@@ -174,7 +174,7 @@ namespace Sistema_de_registro_bibliotecario
         {
             if (string.IsNullOrEmpty(TxtAutor.Text))
             {
-                EpvError.SetError(TxtAutor, "Debe ingresar el autor");
+                EpvError.SetError(TxtAutor, "Debe ingresar un autor");
                 return false;
             }
             else
@@ -188,7 +188,7 @@ namespace Sistema_de_registro_bibliotecario
         {
             if (string.IsNullOrEmpty(TxtBuscarAutor.Text))
             {
-                EpvError.SetError(TxtBuscarAutor, "Debe ingresar un Libro");
+                EpvError.SetError(TxtBuscarAutor, "Debe ingresar un Autor");
                 return false;
             }
             else
